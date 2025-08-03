3f041e35428d4b8cfebe0b1b62678081d8c9900e
Lucas Cardoso RIbeiro <97406660+LucasRib52@users.noreply.github.com>
2025-08-03 02:19:55 -0300

    resolvendo bugs

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,36 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active', 'date_joined']
-        read_only_fields = ['id', 'date_joined'] 
+        read_only_fields = ['id', 'date_joined']
+
+
+class EmailTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        # O campo 'username' pode conter email ou username
+        username_or_email = attrs.get('username')
+        password = attrs.get('password')
+        
+        # Tentar encontrar usuário por email primeiro
+        try:
+            user = User.objects.get(email=username_or_email)
+            # Se encontrou por email, usar o username real
+            attrs['username'] = user.username
+        except User.DoesNotExist:
+            # Se não encontrou por email, tentar por username
+            try:
+                user = User.objects.get(username=username_or_email)
+            except User.DoesNotExist:
+                raise serializers.ValidationError("Usuário não encontrado")
+        
+        # Validar senha
+        if not user.check_password(password):
+            raise serializers.ValidationError("Senha incorreta")
+        
+        # Se chegou até aqui, login válido
+        return super().validate(attrs) 
--- users/views.py
+++ users/views.py
@@ -3,14 +3,15 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.contrib.auth.models import User
-from .serializers import UserSerializer
+from .serializers import UserSerializer, EmailTokenObtainPairSerializer
 
 
 class AdminAuthView(TokenObtainPairView):
     """
-    Login para o painel admin
+    Login para o painel admin - aceita username ou email
     """
     permission_classes = [AllowAny]
+    serializer_class = EmailTokenObtainPairSerializer
 
 
 class AdminAuthRefreshView(TokenRefreshView):
