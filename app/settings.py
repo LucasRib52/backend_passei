3118b5f9256a4d915e73be90bf0ccad0251d173c
Lucas Cardoso RIbeiro <97406660+LucasRib52@users.noreply.github.com>
2025-08-03 01:56:22 -0300

    subindo prod

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8yi_*px%wt7_(wgjseed+gxz-t^jb%6=+7*04u-pzz&#tmj%0d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -128,7 +128,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
 # Default primary key field type
@@ -225,4 +225,3 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Static files configuration
-STATIC_ROOT = BASE_DIR / 'staticfiles'
